f8bd16cfb88b6651be836e41ad232a14da801675
Ivan Brko <ivan.brko@outlook.com>
2018-08-02 21:10:25 +0200

    Working on API

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,15 @@ extern crate zmq;
 //TODO: move this function to it's own module
 fn initialize_zmq() -> (zmq::Context, zmq::Socket, zmq::Socket) {
     let context = zmq::Context::new();
-    let broadcaster = context.socket(zmq::PUSH).unwrap(); //TODO: add error handling here!
-    assert!(broadcaster.bind("tcp:://*:localhost:4567").is_ok()); //TODO: handle this in a nicer way
+    let broadcaster = context.socket(zmq::PUB).unwrap(); //TODO: add error handling here!
+    assert!(broadcaster.bind("tcp://127.0.0.1:5558").is_ok()); //TODO: handle this in a nicer way
 
     let listener = context.socket(zmq::PULL).unwrap(); //TODO: again, handle this in a nicer way
-    assert!(broadcaster.bind("tcp:://*:localhost:4568").is_ok()); //TODO: same thing
+    assert!(listener.bind("tcp://127.0.0.1:4568").is_ok()); //TODO: same thing
 
     (context, broadcaster, listener)
 }
+
 
 fn main() {
     println!("Starting the can-over-zmq broker");
@@ -22,6 +23,8 @@ fn main() {
         let msg = listener.recv_bytes(0).unwrap(); //TODO: check what this flags set, TODO: Error handling!
 
         println!("Got a message, sending it to all connected clients!");
+
+        println!("Message is {:?}", msg);
 
         broadcaster.send(&msg, 0).unwrap(); //TODO: checks flags and error handling
     }
