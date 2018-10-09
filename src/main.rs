ba5cc0892ad64d64849320116c68914dd9066fc1
Ivan Brko <ivan.brko@rimac-automobili.com>
2018-10-09 09:25:04 +0200

    Changed the message printing

--- src/main.rs
+++ src/main.rs
@@ -12,19 +12,37 @@ fn initialize_zmq() -> (zmq::Context, zmq::Socket, zmq::Socket) {
     (context, broadcaster, listener)
 }
 
+fn get_id_from_message(msg: &[u8]) -> u32 {
+    let mut sum: u32 = 0;
+    for (i, v) in msg[..4].iter().rev().enumerate() {
+        sum += 256u32.pow(i as u32) * (*v as u32);
+    }
+    sum
+}
+
+fn print_message(msg: &[u8]) {
+    let mut printed_message = String::from("Message:\tid:");
+    let id = get_id_from_message(msg);
+    printed_message.push_str(&id.to_string());
+    printed_message.push_str("; data: ");
+
+    for msg_byte in msg.iter().skip(4) {
+        printed_message.push_str(&format!("{:X} ", msg_byte))
+    }
+
+    println!("{}", printed_message);
+}
 
 fn main() {
     println!("Starting the can-over-zmq broker");
     let (_context, broadcaster, listener) = initialize_zmq();
-    
+
     loop {
         //TODO: some flag should be added here, when we want to gracefully exit
 
         let msg = listener.recv_bytes(0).unwrap(); //TODO: check what this flags set, TODO: Error handling!
 
-        println!("Got a message, sending it to all connected clients!");
-
-        println!("Message is {:?}", msg);
+        print_message(&msg);
 
         broadcaster.send(&msg, 0).unwrap(); //TODO: checks flags and error handling
     }
